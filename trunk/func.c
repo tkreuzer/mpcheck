04f5e14262eed5f7d6ad95a413b4c5181d4f3a63
zimmerma <zimmerma@c17edfb5-b903-0410-a910-a4031cf99b86>
2005-03-23 18:39:15 +0000

    added support for libmcr added option -dir (only rounding to nearest) cleanep options

--- trunk/func.c
+++ trunk/func.c
@@ -1,5 +1,5 @@
 /*
- * MPCHECK - Check double LIBM functions
+ * MPCHECK - Check mathematical functions
  * Copyright (C) 2002, 2004, 2005 INRIA
  *
  * This program is free software; you can redistribute it and/or modify
@@ -49,11 +49,13 @@ fptype my_div (fptype x, fptype y)
   return x / y;
 }
 
+#ifndef LIBMCR
 #ifdef HAVE_TGAMMA
 fptype my_gamma (fptype x)
 {
   return libmname(tgamma)(x);
 }
+#endif
 #endif
 
 /* Tiny macro */
@@ -61,21 +63,22 @@ fptype my_gamma (fptype x)
 
 mpcheck_func_t  FuncTab[] = {
   {1, func(exp), 0.0, 1.0/0.0, INCREASING, NO_SYMM},
-#ifdef HAVE_EXP2
-  {1, func(exp2), 0.0, 1.0/0.0, INCREASING, NO_SYMM},
-#endif
   {1, func(log), -1.0/0.0, 1.0/0.0, INCREASING, NO_SYMM},
-#ifdef HAVE_LOG2
-  {1, func(log2), -1.0/0.0, 1.0/0.0, INCREASING, NO_SYMM},
-#endif
   {1, func(sin), -1.0, 1.0, NO_MONOTON, ODD},
   {1, func(cos), -1.0, 1.0, NO_MONOTON, EVEN},
   {1, func(tan), -1.0/0.0, 1.0/0.0, NO_MONOTON, ODD},
+  {1, func(atan), -PiRoundedUp/2.0, PiRoundedUp/2.0, INCREASING, ODD},
+  {2, func(pow), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM},
+#ifndef LIBMCR
+#ifdef HAVE_EXP2
+  {1, func(exp2), 0.0, 1.0/0.0, INCREASING, NO_SYMM},
+#endif
+#ifdef HAVE_LOG2
+  {1, func(log2), -1.0/0.0, 1.0/0.0, INCREASING, NO_SYMM},
+#endif
   {1, func(asin), -PiRoundedUp/2.0, PiRoundedUp/2.0, INCREASING, ODD},
   {1, func(acos), 0.0, PiRoundedUp, DECREASING, NO_SYMM},
-  {1, func(atan), -PiRoundedUp/2.0, PiRoundedUp/2.0, INCREASING, ODD},
   {1, func(sqrt), 0.0, 1.0/0.0, INCREASING, NO_SYMM},
-  {2, func(pow), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM},
   {2, func(add), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM},
   {2, func(sub), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM},
   {2, func(mul), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM},
@@ -95,7 +98,8 @@ mpcheck_func_t  FuncTab[] = {
 #endif
   {1, func(cbrt), -1.0/0.0, 1.0/0.0, INCREASING, ODD},
   {2, func(hypot), -1.0/0.0, 1.0/0.0, NO_MONOTON, NO_SYMM}
-#endif
+#endif /* MATHLIB */
+#endif /* LIBMCR */
 };
 
 int FuncTabNum = numberof (FuncTab);
