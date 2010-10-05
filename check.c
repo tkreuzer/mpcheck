04fa86ba480ec8f4ae9eaafef5adf422d63336a5
zimmerma <zimmerma@c17edfb5-b903-0410-a910-a4031cf99b86>
2010-10-05 14:45:13 +0000

    updated copyright years

--- check.c
+++ check.c
@@ -1,6 +1,6 @@
 /*
  * MPCHECK - Check mathematical functions
- * Copyright (C) 2002, 2004, 2005 INRIA
+ * Copyright (C) 2002, 2004, 2005, 2010 INRIA
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
